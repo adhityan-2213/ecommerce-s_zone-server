44f8f8edbc4d04a731dc72bf6fb7399cea8feeee
adhi <adhityaneswari@gmail.com>
2026-06-14 11:32:34 +0530

    Mongo fix

--- server.js
+++ server.js
@@ -106,5 +106,11 @@ if (process.env.NODE_ENV !== "production") {
   startServer(INITIAL_PORT);
 }
 
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB Error:", err));
+  
 // Export for Vercel
 module.exports = app;
